5df65634bd6f46f950caaf2662e7b37d57eddd56
DryRun <david.renhwa.yu@gmail.com>
2020-04-16 17:39:54 -0500

    Debug DR

--- test/runtest.sh
+++ test/runtest.sh
@@ -2,6 +2,6 @@
 crun.py test_Bu2PiJpsi2PiMuMu $MYOMCPATH/test/fragment.py RunIIFall18GS \
     --gfalcp "gsiftp://brux11.hep.brown.edu/mnt/hadoop/store/user/dryu/BParkingMC/test" \
     --keepMini \
-    --nevents_job 10 \
+    --nevents_job 5 \
     --njobs 5 \
     --env
