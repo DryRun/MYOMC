9635399c3b1dfc167f5540f99c93439111c82737
DryRun <david.renhwa.yu@gmail.com>
2020-04-16 16:55:54 -0500

    Test: bring env.tar.gz along

--- test/runtest.sh
+++ test/runtest.sh
@@ -3,4 +3,5 @@ crun.py test_Bu2PiJpsi2PiMuMu $MYOMCPATH/test/fragment.py RunIIFall18GS \
     --gfalcp "gsiftp://brux11.hep.brown.edu/mnt/hadoop/store/user/dryu/BParkingMC/test" \
     --keepMini \
     --nevents_job 10 \
-    --njobs 5
+    --njobs 5 \
+    --env
