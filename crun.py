39ca5388a67976a1e216af7ea171fc8a231f5e96
DryRun <david.renhwa.yu@gmail.com>
2020-04-21 15:51:04 -0500

    Remove B parking nano stuff

--- crun.py
+++ crun.py
@@ -23,7 +23,6 @@ if __name__ == "__main__":
     parser.add_argument("name", type=str, help="Name of output folder (will be created in current directory)")
     parser.add_argument("fragment", type=str, help="Path to fragment")
     parser.add_argument("campaign", type=str, help="Name of campaign")
-    #parser.add_argument("bnano_cfg", type=str, help="Name of BParkingNano cfg")
     parser.add_argument("--env", "-e", action="store_true", help="Use pre-packaged CMSSW environments (run setup_env.sh first)")
     parser.add_argument("--nevents_job", type=int, default=100, help="Number of events per job")
     parser.add_argument("--njobs", type=int, default=1, help="Number jobs")
