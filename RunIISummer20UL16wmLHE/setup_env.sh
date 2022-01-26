e910ef8304a9ea19bbe876f75d2516ff35c2fd4e
DryRun <david.renhwa.yu@gmail.com>
2022-01-25 18:31:11 -0600

    Add more UL campaigns

---
+++ RunIISummer20UL16wmLHE/setup_env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Some old stuff to ensure this is run on SLC6
+#export SYSTEM_RELEASE=`cat /etc/redhat-release`
+#if { [[ $SYSTEM_RELEASE == *"release 7"* ]]; }; then
+#  echo "Running setup_env.sh on SLC6."
+#  if { [[ $(hostname -s) = lxplus* ]]; }; then
+#  	ssh -Y lxplus6 "cd $PWD; source setup_env.sh;"
+#  elif { [[ $(hostname -s) = cmslpc* ]]; }; then
+#  	ssh -Y cmslpc-sl6 "cd $PWD; source setup_env.sh;"
+#  else
+#  	echo "Not on cmslpc or lxplus, not sure what to do."
+#  	return 1
+#  fi
+#  return 1
+#fi
+
+mkdir env
+cd env
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+scram project -n "CMSSW_10_6_28_patch1" CMSSW_10_6_28_patch1
+cd CMSSW_10_6_28_patch1/src
+eval `scram runtime -sh`
+scram b
+cd ../..
+
+scram project -n "CMSSW_10_6_17_patch1" CMSSW_10_6_17_patch1
+cd CMSSW_10_6_17_patch1/src
+eval `scram runtime -sh`
+scram b
+cd ../..
+
+scram project -n "CMSSW_8_0_33_UL" CMSSW_8_0_33_UL
+cd CMSSW_8_0_33_UL/src
+eval `scram runtime -sh`
+scram b
+cd ../..
+
+scram project -n "CMSSW_10_6_25" CMSSW_10_6_25
+cd CMSSW_10_6_25/src
+eval `scram runtime -sh`
+scram b
+cd ../../
+
+tar -czvf env.tar.gz ./CMSSW*
+mv env.tar.gz ..
+cd ..
