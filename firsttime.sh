b6943b7be15958e8a0124a7158d8d1cd98c04a87
DryRun <david.renhwa.yu@gmail.com>
2022-01-25 18:41:04 -0600

    Update default campaigns

--- firsttime.sh
+++ firsttime.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source env.sh
 TOPDIR=$PWD
-CAMPAIGNS=( "RunIIFall18GS" "RunIIFall18GSBParking" "RunIIFall18wmLHEGS" )
+#CAMPAIGNS=( "RunIIFall18GS" "RunIIFall18GSBParking" "RunIIFall18wmLHEGS" )
+CAMPAIGNS=( "RunIISummer20UL16wmLHE" "RunIISummer20UL16APVwmLHE" "RunIISummer20UL17wmLHE" "RunIISummer20UL18wmLHE" "NANOGEN" )
 for CAMPAIGN in "${CAMPAIGNS[@]}"; do
 	cd $CAMPAIGN
 	source setup_env.sh
