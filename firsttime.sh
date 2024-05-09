92ef8be2cf89c6c7faedbe528a0561249601f84a
David Yu <david.renhwa.yu@gmail.com>
2024-05-09 17:49:56 -0500

    Correct OS in firsttime.sh

--- firsttime.sh
+++ firsttime.sh
@@ -6,7 +6,15 @@ CAMPAIGNS=( "RunIISummer20UL16wmLHE" "RunIISummer20UL16APVwmLHE" "RunIISummer20U
 #CAMPAIGNS=( "RunIISummer20UL16wmLHE_pfnano" "RunIISummer20UL16APVwmLHE_pfnano" "RunIISummer20UL17wmLHE_pfnano" "RunIISummer20UL18wmLHE_pfnano" "NANOGEN" )
 for CAMPAIGN in "${CAMPAIGNS[@]}"; do
 	cd campaigns/$CAMPAIGN
-	source setup_env.sh
+	#source setup_env.sh
+	if [[ $CAMPAIGN == *"RunIISummer"* ]]; then
+		cmssw-el7 -- setup_env.sh
+	elif [[ $CAMPAIGN == *"Run3"* ]]; then
+		cmssw-el8 -- setup_env.sh
+	else
+		echo "I don't know what OS to use for campaign ${CAMPAIGN}. Please fix firsttime.sh."
+		exit 1
+	fi
 	source getpileupfiles.sh
 	cd $TOPDIR
 done
