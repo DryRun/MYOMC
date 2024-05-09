6285e640eabf0f47f515937f922922413694e1d0
David Yu <david.renhwa.yu@gmail.com>
2024-05-09 18:31:59 -0500

    Bind proxy dir in apptainer

--- firsttime.sh
+++ firsttime.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-source env.sh
+if [ -z ${MYOMCPATH+x} ]; then
+	source env.sh
+fi
+voms-proxy-init -voms cms
 TOPDIR=$PWD
 #CAMPAIGNS=( "RunIIFall18GS" "RunIIFall18GSBParking" "RunIIFall18wmLHEGS" )
 CAMPAIGNS=( "RunIISummer20UL16wmLHE" "RunIISummer20UL16APVwmLHE" "RunIISummer20UL17wmLHE" "RunIISummer20UL18wmLHE" "NANOGEN" )
 #CAMPAIGNS=( "RunIISummer20UL16wmLHE_pfnano" "RunIISummer20UL16APVwmLHE_pfnano" "RunIISummer20UL17wmLHE_pfnano" "RunIISummer20UL18wmLHE_pfnano" "NANOGEN" )
 for CAMPAIGN in "${CAMPAIGNS[@]}"; do
-	cd campaigns/$CAMPAIGN
-	#source setup_env.sh
-	
+	cd campaigns/$CAMPAIGN	
 	if [[ $CAMPAIGN == *"RunIISummer"* ]]; then
-		cmssw-el7 -p --bind `readlink -f ${PWD}` -- ./setup_env.sh
+		cmssw-el7 -p --bind `readlink -f ${PWD}` --bind `readlink $HOME/private` -- ./setup_env.sh
 	elif [[ $CAMPAIGN == *"NANOGEN"* ]]; then
-		cmssw-el7 -p --bind `readlink -f ${PWD}` -- ./setup_env.sh
+		cmssw-el7 -p --bind `readlink -f ${PWD}` --bind `readlink $HOME/private` -- ./setup_env.sh
 	elif [[ $CAMPAIGN == *"Run3"* ]]; then
-		cmssw-el8 -p --bind `readlink -f ${PWD}` -- ./setup_env.sh
+		cmssw-el8 -p --bind `readlink -f ${PWD}` --bind `readlink $HOME/private` -- ./setup_env.sh
 	else
 		echo "I don't know what OS to use for campaign ${CAMPAIGN}. Please fix firsttime.sh."
 		exit 1
